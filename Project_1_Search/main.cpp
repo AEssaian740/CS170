1d8a59dd28aef0befbf5fc5901381bfc03d66b9a
AEssaian740 <aessa002@ucr.edu>
2022-05-01 10:57:31 -0700

    Base files for Project 1 Search

---
+++ Project_1_Search/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cmath>
+#include "node.h"
+
+using namespace std;
+
+// TODO
+//*Build the 8-puzzle
+//*Implement Uniform Cost Search
+//*Implement A* with Misplaced Tile hueristic
+//*Implement A* with Euclidean Distance hueristic
+
+int main() {
+
+}
